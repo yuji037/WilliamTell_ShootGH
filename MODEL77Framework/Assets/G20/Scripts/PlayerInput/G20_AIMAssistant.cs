3d7ad6b572a518bb7dd7279017257f73a7e3dc93
umeigon <umeryosan@gmail.com>
2018-08-30 13:41:05 +0900

    titleappleを撃つまでのshotcountで、AIM補正の値を変更するようにに修正

--- MODEL77Framework/Assets/G20/Scripts/PlayerInput/G20_AIMAssistant.cs
+++ MODEL77Framework/Assets/G20/Scripts/PlayerInput/G20_AIMAssistant.cs
@@ -6,7 +6,7 @@ public class G20_AIMAssistant
 {
     public Vector2 AssistAIM(Vector2 shot_point, float assist_value)
     {
-        float shortestDistance = 30000f;
+        float shortestDistance = 3000000f;
         Vector2 nearestDiff = Vector2.zero;
         foreach (var e in G20_EnemyCabinet.GetInstance().EnemyList)
         {
@@ -16,14 +16,13 @@ public class G20_AIMAssistant
         }
         //AIM補正のポイントがターゲットを通り過ぎないようにする
         if (assist_value > shortestDistance) assist_value = shortestDistance;
-
         return shot_point + (nearestDiff.normalized * assist_value);
     }
     void ChangeNearest(ref Vector2 nearest_diff,ref float shortest_distance, Vector2 shot_point, Vector3 target_postion)
     {
         Vector2 targetPoint = Camera.main.WorldToScreenPoint(target_postion);
         var diffVec = targetPoint - shot_point;
-        var dis = diffVec.magnitude;
+        var dis = diffVec.sqrMagnitude;
         //最短距離じゃなければnullを返す
         if (shortest_distance > dis) {
             shortest_distance = dis;
