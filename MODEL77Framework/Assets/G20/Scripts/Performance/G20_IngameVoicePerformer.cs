62004d76b195ce5f081b251d6193f9a838570f9e
umeigon <umeryosan@gmail.com>
2018-09-04 00:21:05 +0900

    インゲームの演出prefab追加

---
+++ MODEL77Framework/Assets/G20/Scripts/Performance/G20_IngameVoicePerformer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G20_IngameVoicePerformer : G20_IngamePerformer
+{
+    [SerializeField] G20_VoiceType voiceType;
+    public override void StartPerformance()
+    {
+        Debug.Log("voicePlay");
+        G20_VoicePerformer.GetInstance().Play(voiceType);
+    }
+}
