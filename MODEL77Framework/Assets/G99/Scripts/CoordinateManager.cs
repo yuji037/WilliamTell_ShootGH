7f9529f43cbce800a48ed0e0f3884a11c377286d
yuji037 <pnst_n_th_wr037@yahoo.co.jp>
2018-09-04 15:39:35 +0900

    Revert "Github移行の為、いくつか必要なファイルを削除（プルするとゲーム動かなくなるので注意）"
    This reverts commit bfd05cf0b1d0653039d69de36db087f595638937.

---
+++ MODEL77Framework/Assets/G99/Scripts/CoordinateManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoordinateManager : MonoBehaviour {
+
+	private bool is_input;
+	private Vector3 mouse_pos;
+
+	//ST84
+	private bool hitsts;
+	private double x = 0f;
+	private double y = 0f;
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0) && is_input)
+		{
+			is_input = false;
+			hitsts = true;
+			mouse_pos = Input.mousePosition;
+			x = mouse_pos.x;
+			y = mouse_pos.y;
+		}
+		if (Input.GetMouseButtonUp(0) && !is_input)
+		{
+			is_input = true;
+		}
+	}
+
+	// 座標の取得
+	public Hashtable Get()
+	{
+		var ret = new Hashtable();
+		ret.Add("x", (float)x);
+		ret.Add("y", (float)y);
+		hitsts = false;
+		return ret;
+	}
+
+	// 着弾情報があるかのチェック
+	public bool isUpdate()
+	{
+		return hitsts;
+	}
+}
---
+++ MODEL77Framework/Assets/G99/Scripts/G99_GameManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class G99_GameManager : MonoBehaviour
+{
+	// フレームワークの関数を使うために必要
+	private GameController _gameController;
+	private CoordinateManager _coordinateManager;
+
+	// GameController.cs から値をもらって保存しておく変数
+	private int playerNum;
+	private string[] playerId;
+
+	private G99_UIView _uiView;
+
+	[SerializeField]
+	private int _targetCount = 5;
+	public int TargetCount {
+		get { return _targetCount; }
+		set { _targetCount = value; }
+	}
+
+	private bool _isGameEnd;
+
+	[SerializeField]
+	private GameObject _hitEffect;
+
+	void Start()
+	{
+		_gameController = GameObject.Find("GameManager").GetComponent<GameController>();
+		_coordinateManager = GameObject.Find("GameManager").GetComponent<CoordinateManager>();
+
+		_uiView = this.GetComponent<G99_UIView>();
+	}
+
+	void Update()
+	{
+		// 画面に弾がヒットした場合 isUpdate() はtrueを返す。
+		if (_coordinateManager.isUpdate() && !_isGameEnd)
+		{
+			// CoordinateManagerから情報を取得。
+			Hashtable res = _coordinateManager.Get();
+			Vector3 pos = new Vector3((float)res["x"], (float)res["y"], 0f);
+
+			// 受け取った座標をRayに変換
+			Ray ray = Camera.main.ScreenPointToRay(pos);
+
+			// Raycast処理
+			RaycastHit hit = new RaycastHit();
+			if (Physics.Raycast(ray, out hit))
+			{
+				GameObject hitObj = hit.collider.gameObject;
+
+				PlayEffect(pos);
+
+				if (hitObj.name == "Target")
+					_targetCount--;
+
+				_uiView.SetTargetCount(_targetCount);
+
+				if (_targetCount <= 0)
+				{
+					_isGameEnd = true;
+					_uiView.DisplayGameEnd();
+
+					// scoreをGameControllerに送信。
+					SendScore();
+
+					// ゲームの終わりを通知し、メニュー画面へ
+					_gameController.GameEnd();
+				}
+			}
+		}
+	}
+
+	public void PlayEffect(Vector3 pos)
+	{
+		GameObject eff = Instantiate(_hitEffect);
+		pos.z = 18f;
+		eff.transform.position = Camera.main.ScreenToWorldPoint(pos);
+	}
+
+	public void SendScore()
+	{
+		var score = new int[1];
+		score[0] = 10;
+		var idm = playerId;
+		var idate = new string[1];
+		idate[0] = _gameController.Now();
+
+		// 順番は配列の0番目から順にプレイヤー1, プレイヤー2・・・となる。
+		// id: プレイヤーの識別番号
+		// score: プレイヤーのスコア
+		// idate: スコア送信時刻
+		_gameController.ScoreUpdate(idm, score, idate);
+	}
+}
