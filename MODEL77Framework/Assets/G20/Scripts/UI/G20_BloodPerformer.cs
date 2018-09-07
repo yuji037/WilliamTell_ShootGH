15ea3061a0a7107a816033deae4dee22ae059931
umei <umeryosan@gmail.com>
2018-09-07 12:29:46 +0900

    血糊演出追加

---
+++ MODEL77Framework/Assets/G20/Scripts/UI/G20_BloodPerformer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G20_BloodPerformer : MonoBehaviour
+{
+    [SerializeField] GameObject[] bloodPanel;
+    [SerializeField] G20_Player player;
+    int damageCount=0;
+    // Update is called once per frame
+    void Start()
+    {
+        player.recvDamageActions += ReceivedDamage;
+    }
+    void ReceivedDamage(G20_Unit _unit)
+    {
+        if(CheckBloodPanel(damageCount))bloodPanel[damageCount].SetActive(true);
+        damageCount++;
+    }
+    bool CheckBloodPanel(int num)
+    {
+        return (0 <= num && num < bloodPanel.Length);
+    }
+}
