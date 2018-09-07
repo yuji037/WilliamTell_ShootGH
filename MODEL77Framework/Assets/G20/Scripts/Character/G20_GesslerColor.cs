f63b2214d43f6380063792b2cff5e1d602870109
yuji037 <pnst_n_th_wr037@yahoo.co.jp>
2018-09-07 17:41:25 +0900

    ゲスラーLive2Dのアニメーションをシーンに追加

---
+++ MODEL77Framework/Assets/G20/Scripts/Character/G20_GesslerColor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Live2D.Cubism.Rendering;
+
+public class G20_GesslerColor : MonoBehaviour {
+
+    public float Color = 1f;
+    private float _color = 1f;
+
+    CubismRenderer[] cubismRenderers;
+
+    private void Start()
+    {
+        cubismRenderers = GetComponentsInChildren<CubismRenderer>();
+    }
+
+    private void Update()
+    {
+        if(_color != Color )
+        {
+            foreach (var cr in cubismRenderers )
+            {
+                Debug.Log("iro : " + Color);
+                cr.Color = new Color(Color, Color, Color, 1f);
+            }
+            _color = Color;
+        }
+    }
+}
