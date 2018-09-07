030134642812bcff7442f283d3aec9a55a08f6fa
yuji037 <pnst_n_th_wr037@yahoo.co.jp>
2018-09-08 03:26:17 +0900

    クリア時画面の修正

--- MODEL77Framework/Assets/G20/Scripts/GameSystem/G20_ClearPerformer.cs
+++ MODEL77Framework/Assets/G20/Scripts/GameSystem/G20_ClearPerformer.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.UI;
-public class G20_ClearPerformer : G20_Singleton<G20_ClearPerformer>
-{
+
+public class G20_ClearPerformer : G20_Singleton<G20_ClearPerformer> {
     //この値を変えることでランダムに降らすか整列して降らすかを変えられる
     [SerializeField] bool IsRandomlyFall;
     [SerializeField] GameObject appleObj;
@@ -13,27 +13,22 @@ public class G20_ClearPerformer : G20_Singleton<G20_ClearPerformer>
     [SerializeField] float fallAppleDelay;
     [SerializeField] GameObject paramObjs;
 
-    
+
     [SerializeField] GameObject[] onClearDeactivateFieldObjs;
     [SerializeField] GameObject[] onClearActivateFieldObjs;
 
     [SerializeField] MeshRenderer backGroundMesh;
     [SerializeField] Material clearBackGroundMaterial;
 
-    [SerializeField] GameObject skyObject;
-    [SerializeField] Light directionLight;
-    [SerializeField] GameObject gesuraObj;
-    [SerializeField] Light directionLight2;
-
     [SerializeField] Text yourScore;
     [SerializeField] Text creatorsHighScore;
     [SerializeField] Text dailyHighScore;
 
     [SerializeField] GameObject clearTexts;
-    
 
-    
-    [SerializeField] float fadetime=0;
+
+
+    [SerializeField] float fadetime = 0;
 
 
     float balanceNum;
@@ -44,7 +39,7 @@ public class G20_ClearPerformer : G20_Singleton<G20_ClearPerformer>
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
-        Gizmos.DrawWireCube(fallPoint,fallSize*2);
+        Gizmos.DrawWireCube(fallPoint, fallSize * 2);
     }
     IEnumerator FallAppleRoutine(Action on_end_action)
     {
@@ -54,17 +49,17 @@ public class G20_ClearPerformer : G20_Singleton<G20_ClearPerformer>
         yield return new WaitForSeconds(0.1f);
         G20_FadeChanger.GetInstance().StartWhiteFadeIn(2.0f);
         yield return new WaitForSeconds(2.0f);
-        
+
         var apple_value = G20_Score.GetInstance().Score;
         StartCoroutine(UIRoutine(apple_value));
 
         //リンゴ積み上げ
         balanceNum = -fallSize.x;
-        for (int i = 0; i < apple_value; i++)
+        for ( int i = 0; i < apple_value; i++ )
         {
             var apple = Instantiate(appleObj);
             apple.transform.SetParent(transform);
-            if (IsRandomlyFall)
+            if ( IsRandomlyFall )
             {
                 apple.transform.position = GetRandomPosition();
             }
@@ -74,7 +69,7 @@ public class G20_ClearPerformer : G20_Singleton<G20_ClearPerformer>
             }
             yield return new WaitForSeconds(fallAppleDelay);
         }
-        if (on_end_action != null) on_end_action();
+        if ( on_end_action != null ) on_end_action();
     }
 
 
@@ -96,19 +91,20 @@ public class G20_ClearPerformer : G20_Singleton<G20_ClearPerformer>
 
     //ホワイトアウトしている間にする処理
     void SetClearObjects()
-        {
-            paramObjs.SetActive(false);
-            backGroundMesh.material = clearBackGroundMaterial;
+    {
+        paramObjs.SetActive(false);
+        backGroundMesh.material = clearBackGroundMaterial;
+        backGroundMesh.enabled = true;
 
-            foreach (var obj in onClearDeactivateFieldObjs)
-            {
-                obj.SetActive(false);
-            }
-            foreach (var obj in onClearActivateFieldObjs)
-            {
-                obj.SetActive(true);
-            }
+        foreach ( var obj in onClearDeactivateFieldObjs )
+        {
+            obj.SetActive(false);
         }
+        foreach ( var obj in onClearActivateFieldObjs )
+        {
+            obj.SetActive(true);
+        }
+    }
 
 
     void SetUIsActive()
@@ -117,9 +113,9 @@ public class G20_ClearPerformer : G20_Singleton<G20_ClearPerformer>
     }
     Vector3 GetBalancePosition()
     {
-        Vector3 retPos = fallPoint + new Vector3(balanceNum,0,0);
-        balanceNum+=0.5f;
-        if (balanceNum>fallSize.x)
+        Vector3 retPos = fallPoint + new Vector3(balanceNum, 0, 0);
+        balanceNum += 0.5f;
+        if ( balanceNum > fallSize.x )
         {
             balanceNum = -fallSize.x;
         }
@@ -127,7 +123,7 @@ public class G20_ClearPerformer : G20_Singleton<G20_ClearPerformer>
     }
     Vector3 GetRandomPosition()
     {
-        var randPos = new Vector3( UnityEngine.Random.Range(-fallSize.x, fallSize.x), UnityEngine.Random.Range (- fallSize.y, fallSize.y), UnityEngine.Random.Range( - fallSize.z, fallSize.z));
-        return randPos+fallPoint;
+        var randPos = new Vector3(UnityEngine.Random.Range(-fallSize.x, fallSize.x), UnityEngine.Random.Range(-fallSize.y, fallSize.y), UnityEngine.Random.Range(-fallSize.z, fallSize.z));
+        return randPos + fallPoint;
     }
 }
