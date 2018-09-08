53d99f4047e28eecdd2fbb5ed4cf0813c9928de3
yuji037 <pnst_n_th_wr037@yahoo.co.jp>
2018-09-08 19:51:34 +0900

    BGM変更機能、PostProcessing入れてみた

---
+++ MODEL77Framework/Assets/G20/Scripts/Performance/G20_IngameBGMChanger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G20_IngameBGMChanger : G20_IngamePerformer {
+
+    [SerializeField]
+    G20_BGMType playBGMType;
+
+    public override void StartPerformance()
+    {
+        G20_BGMManager.GetInstance().Play(playBGMType);
+    }
+}
