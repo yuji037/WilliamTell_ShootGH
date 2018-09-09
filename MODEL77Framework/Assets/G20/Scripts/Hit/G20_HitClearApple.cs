7706a2419687900d30b84ec1b8471fd99799e415
yuji037 <pnst_n_th_wr037@yahoo.co.jp>
2018-09-10 01:21:32 +0900

    ClearApple修正

--- MODEL77Framework/Assets/G20/Scripts/Hit/G20_HitClearApple.cs
+++ MODEL77Framework/Assets/G20/Scripts/Hit/G20_HitClearApple.cs
@@ -6,8 +6,18 @@ public class G20_HitClearApple : G20_HitAction {
 
     [SerializeField] float colliderExpansionRate = 10.0f;
 
+    // 爆発に見せかけた膨張
     public override void Execute(Vector3 hit_point)
     {
-        GetComponent<SphereCollider>().radius *= colliderExpansionRate;
+        var col = GetComponent<SphereCollider>();
+        col.radius *= colliderExpansionRate;
+        StartCoroutine(ColliderOffCoroutine(col));
+    }
+
+    // 一度膨張したものが再び床に乗ると浮いてしまうので、すり抜けさせる
+    IEnumerator ColliderOffCoroutine(Collider col)
+    {
+        yield return new WaitForSeconds(0.2f);
+        col.enabled = false;
     }
 }
