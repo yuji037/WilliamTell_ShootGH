8f79e0c7fe4dfa8eac615b534a2ca269e2a8a50e
umeigon <umeryosan@gmail.com>
2018-09-09 22:43:16 +0900

    UIのライフを変更

--- MODEL77Framework/Assets/G20/Scripts/UI/G20_UIPlayerLife.cs
+++ MODEL77Framework/Assets/G20/Scripts/UI/G20_UIPlayerLife.cs
@@ -1,19 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 //lifeがゲーム中10くらい回復すると厳しい設計
 public class G20_UIPlayerLife : MonoBehaviour {
-    [SerializeField]List<GameObject> lifeImages=new List<GameObject>();
+    [SerializeField]List<Image> lifeImages=new List<Image>();
     [SerializeField] G20_Player player;
+    [SerializeField] float fadeDuration;
     int activeCount=0;
     // Use this for initialization
 	void Start () {
         player.recvDamageActions += ChangeLife;
         int pLife= player.HP;
-        for (int i=0;i<pLife;i++)
+        for (int i=0;i<pLife-1;i++)
         {
-            lifeImages[i].SetActive(true);
+            lifeImages[i].gameObject.SetActive(true);
             activeCount++;
         }
     }
@@ -21,21 +22,28 @@ public class G20_UIPlayerLife : MonoBehaviour {
     {
         if (_unit.HP < 0) return;
         //増やす
-        while (lifeImages.Count < _unit.HP)
+        while (lifeImages.Count < _unit.HP-1)
         {
-            lifeImages[activeCount].SetActive(true);
+            lifeImages[activeCount].gameObject.SetActive(true);
             activeCount++;
         }
+        if (_unit.HP <= 0) return;
         //減らす
-        while (activeCount> _unit.HP)
+        while (activeCount> _unit.HP-1)
         {
             var img=lifeImages[0];
             lifeImages.RemoveAt(0);
-            Destroy(img);
+            StartCoroutine(lifeImageFade(img));
             activeCount--;
         }
     }
-	// Update is called once per frame
-	void Update () {
-	}
+    IEnumerator lifeImageFade(Image img)
+    {
+        for (float t=0;t<fadeDuration;t+=Time.deltaTime)
+        {
+            img.fillAmount = 1.0f-Mathf.Lerp(0,1,t/fadeDuration);
+            yield return null;
+        }
+        Destroy(img);
+    }
 }
