e9ffd89607ea2330760478b273853c5151f25c8c
umeigon <umeryosan@gmail.com>
2018-09-10 00:21:30 +0900

    UIの演出追加、FPSCountr追加

---
+++ MODEL77Framework/Assets/G20/Scripts/Debug/G20_FPSCounter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class G20_FPSCounter : MonoBehaviour
+{
+    [SerializeField]float CalcDuration;
+    [SerializeField] Text fpsText;
+    [SerializeField]float showFPSDuration;
+    Coroutine fpsCoroutine;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(CountFPS());
+    }
+    IEnumerator CountFPS()
+    {
+        while (true)
+        {
+            int frameCount = 0;
+            float timer = 0f;
+            while (CalcDuration >= timer)
+            {
+                frameCount++;
+                timer += Time.deltaTime;
+                yield return null;
+            }
+            //FPS算出
+            float fps=(frameCount / timer);
+            if (fps<60.0f)
+            {
+                fpsText.text = "FPS:" +(int)fps;
+                //前のコルーチンを削除
+                if (fpsCoroutine != null) StopCoroutine(fpsCoroutine);
+                //表示コルーチン
+                fpsCoroutine =StartCoroutine(ShowFPS(fps));
+            }
+        }
+    }
+    IEnumerator ShowFPS(float _fps)
+    {
+        fpsText.enabled = true;
+        yield return new WaitForSeconds(showFPSDuration);
+        fpsText.enabled = false;
+    }
+}
