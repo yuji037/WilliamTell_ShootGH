1f519152a8466eb830975ae3ec558d25388d2661
umeigon <umeryosan@gmail.com>
2018-09-09 20:36:27 +0900

    bombを倒すと落ちるように修正

---
+++ MODEL77Framework/Assets/G20/Scripts/Character/G20_BombPositionFix.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//死んだときにpivotのpositionまで1秒かけて戻る
+public class G20_BombPositionFix : MonoBehaviour {
+    [SerializeField] Transform model;
+    [SerializeField] Transform pivot;
+	// Use this for initialization
+	void Start () {
+        GetComponent<G20_Enemy>().deathActions += _=>StartCoroutine(FixPosition());
+    }
+	IEnumerator FixPosition()
+    {
+        var startPos = pivot.position;
+        var fixVec=pivot.position-model.position;
+        fixVec = fixVec.normalized;
+        var dis = Vector3.Distance(pivot.position, model.position);
+        for (float t = 0; t < 1.0f; t += Time.deltaTime)
+        {
+            pivot.position=startPos+ fixVec*Mathf.Lerp(0,dis,t);
+            if (model.position.y <= 0) yield break;
+            yield return null;
+        }
+    }
+}
