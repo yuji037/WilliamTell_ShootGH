f0eea658060c0c9ffcc7e778c1f18cb786c93506
yuji037 <pnst_n_th_wr037@yahoo.co.jp>
2018-09-09 19:24:10 +0900

    PostProcessingの調整、LightMapでグローバルイルミネーション試しに導入、木のアニメーション導入

---
+++ MODEL77Framework/Assets/G20/Scripts/Performance/G20_TreePerformer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G20_TreePerformer : MonoBehaviour {
+
+    [SerializeField]
+    float animInterval = 5.0f;
+
+    Animator[] animators;
+
+    float nextInterval = 0f;
+    float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+        animators = GetComponentsInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if ( nextInterval <= timer )
+        {
+            timer = 0;
+
+            int num = Random.Range(0, animators.Length);
+            animators[num].CrossFade("WindReceive", 0.4f);
+            animators[num].speed = Random.Range(0.1f, 0.2f);
+
+            float intervalMin = animInterval - 0.5f;
+            float intervalMax = animInterval + 0.5f;
+            if ( intervalMin < 0 ) intervalMin = 0;
+            if ( intervalMax < 0 ) intervalMax = 0;
+
+            nextInterval = Random.Range(intervalMin, intervalMax);
+
+        }
+    }
+
+
+}
