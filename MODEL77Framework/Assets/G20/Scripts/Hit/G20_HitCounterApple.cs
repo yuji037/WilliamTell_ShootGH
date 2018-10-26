fa845250999cd86ca8d7a59fc4d026a1707ad1ad
umeigon <umeryosan@gmail.com>
2018-10-26 20:24:16 +0900

    ゲスラー戦追加

---
+++ MODEL77Framework/Assets/G20/Scripts/Hit/G20_HitCounterApple.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G20_HitCounterApple : G20_HitAction {
+    
+    public override void Execute(Vector3 hit_point)
+    {
+        G20_BulletAppleCreator.GetInstance().Create(hit_point);
+    }
+}
--- MODEL77Framework/Assets/G20/Scripts/Hit/G20_HitEffect.cs
+++ MODEL77Framework/Assets/G20/Scripts/Hit/G20_HitEffect.cs
@@ -7,10 +7,12 @@ public class G20_HitEffect : G20_HitAction
 
     [SerializeField]G20_EffectType effectType;
     [SerializeField]bool isCorrect=true;
+    public Transform effctParent;
     public override void Execute(Vector3 hit_point)
     {
         var effect = G20_EffectManager.GetInstance().Create(effectType, hit_point);
-        if(isCorrect)effect.transform.position = G20_PositionCorrector.Correct(effect.transform.position);
+        if (effctParent != null) effect.transform.parent = effctParent;
+        if (isCorrect)effect.transform.position = G20_PositionCorrector.Correct(effect.transform.position);
     }
     // 当たるたびにエフェクト変える場合は
     // これではなくG20_HitMultiEffectを使う
