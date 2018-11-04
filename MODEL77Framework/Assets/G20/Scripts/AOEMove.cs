a26ecad1f531ea24fff48b7d77ed70ac5b7fc009
ganigani512 <ganiganick512@gmail.com>
2018-11-04 16:30:25 +0900

    ボムアプルにAOE追加

---
+++ MODEL77Framework/Assets/G20/Scripts/AOEMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AOEMove : MonoBehaviour {
+
+
+    [SerializeField]
+    GameObject bomb;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(parentChange());
+	}
+	
+    IEnumerator parentChange()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        transform.parent = bomb.transform.parent;
+
+        yield return null;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
